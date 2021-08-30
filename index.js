adb0c0888d8b7441f18886fdd297dc7aad0ef0c8
Bo Wang <bo.wang@albelli.com>
2021-08-30 11:52:22 +0200

    first

---
+++ index.js
@@ -0,0 +1,19 @@
+const core = require('@actions/core');
+const { runTest } = require('./lib/run-test');
+
+(async function () {
+  try {
+    let tag = core.getInput('tag');
+    console.log(`Run tests on Tag ${tag}!`);
+
+    let resultsPath= await runTest({
+      tag
+    });
+
+    core.setOutput("results-path", resultsPath);
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
